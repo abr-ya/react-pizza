181eae018e71dc7ee80a866cbbb721f53546cca6
abr-ya <gamer05@list.ru>
2020-08-09 18:15:42 +0300

    Delivery component

--- src/components/Delivery/Delivery.tsx
+++ src/components/Delivery/Delivery.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
 import styles from './delivery.module.scss';
 
-const Delivery = () => (
-	<div className={`container-fluid ${styles.delivery}`}>
-		<div className='row'>
-			<div className='col-md-12'>
-				<h2>Доставка и оплата</h2>
+interface IDeliveryCard {
+	title: string;
+	image: string;
+	text: string;
+};
+
+const Delivery = () => {
+	// данные пока хардкодим прямо тут
+	const DeliveryData: IDeliveryCard[] = [
+		{
+			title: 'Заказ',
+			image: 'icon-order.svg',
+			text: 'После оформления заказа мы свяжемся с вами для уточнения деталей.',
+		},
+		{
+			title: 'Доставка курьером',
+			image: 'icon-delivery.svg',
+			text: 'Мы доставим вашу пиццу горячей. Бесплатная доставка по городу.',
+		},
+		{
+			title: 'Оплата',
+			image: 'icon-pay.svg',
+			text: 'Оплатить можно наличными или картой курьеру. И золотом тоже можно.',
+		},
+	]
+
+	return (
+		<div className={`container-fluid ${styles.delivery}`}>
+			<div className="container">
+				<div className='row'>
+					<div className='col-md-12'>
+						<h2>Доставка и оплата</h2>
+					</div>
+				</div>
+				<div className='row'>
+					{DeliveryData.map((item: IDeliveryCard) => (
+						<div className={`col-md-4 ${styles.wrapper}`}>
+							<div className={styles.card}>
+								<div className={styles.left}>
+									<img src={`img/${item.image}`} alt="icon-order"/>
+								</div>
+								<div className={styles.right}>
+									<h3>{item.title}</h3>
+									{item.text}
+								</div>
+							</div>
+						</div>					
+					))}
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+}
 
 export default Delivery;
