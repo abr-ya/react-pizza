bc506c701e1d80e930d189c7e7c73d55a6f4fae6
abr-ya <gamer05@list.ru>
2020-08-30 18:16:02 +0300

    Update Inst component

--- src/components/Inst/Inst.tsx
+++ src/components/Inst/Inst.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import styles from './inst.module.scss';
 
-const Inst = () => (
-	<div className={`container-fluid ${styles.inst}`}>
-		<div className='row'>
-			<div className='col-md-12'>
-				<h2>Следите за нами в Instagram</h2>
+const Inst = () => {
+	const img: string[] = ['01', '02', '03', '04', '06', '07', '08', '09'];
+
+	return (
+		<div className={`container-fluid ${styles.inst}`}>
+			<div className='row'>
+				<div className='col-md-12'>
+					<h2>Следите за нами в Instagram</h2>
+					<span className={styles.pizzamenu}>@pizzamenu</span>
+				</div>
+				<div className='col-md-12'>
+					{img.map((item: string, index) => (
+						<div className={styles.imgWrapper} key={index}>
+							<img src={`img/inst-${item}.jpg`} alt={`inst-${item}`}/>
+							
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Inst;
